3b31d4d686799f364600419183efbb4e4b024963
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-03-29 21:45:02 -0500

    Add CORS configuration to enable access from any origin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT) || 3000;
+
+  app.enableCors({
+    origin: '*', // Permite acceso desde cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(port);
 }
 bootstrap();
